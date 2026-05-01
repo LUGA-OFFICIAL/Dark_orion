43d496bbfad1efb1c90559476a94b7f04626c275
LUGA <74767308+LUGA-OFFICIAL@users.noreply.github.com>
2026-05-01 16:09:28 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from telegram.ext import Updater, CommandHandler
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+def start(update, context):
+    update.message.reply_text("🔥 شغال")
+
+def main():
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    print("🔥 RUNNING NEW FILE")
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
